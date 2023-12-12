76e3a19c6ed20967e1583780e8863ba164486dad
José Bonança Pedreira <72609286+joko1712@users.noreply.github.com>
2023-12-12 14:02:11 +0000

    Code update

--- hybodesolver.py
+++ hybodesolver.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def hybodesolver(odesfun, controlfun, eventfun, t0, tf, state, jac, hess, w, batch, projhyb):
+def hybodesolver(ann,odesfun, controlfun, eventfun, t0, tf, state, jac, hess, w, batch, projhyb):
     t = t0
     hopt = []
 
@@ -20,24 +20,24 @@ def hybodesolver(odesfun, controlfun, eventfun, t0, tf, state, jac, hess, w, bat
         if jac is not 0:
             k1_state, k1_jac = odesfun(t, state, jac, None, w, ucontrol1, projhyb)
         else:
-            k1_state = odesfun(t, state, None, None, w, ucontrol1, projhyb)
+            k1_state = odesfun(ann,t, state, None, None, w, ucontrol1, projhyb)
 
         ucontrol2 = controlfun(t + h / 2, batch) if controlfun is not None else []
 
         if jac is not 0:
-            k2_state, k2_jac = odesfun(t + h / 2, state + h / 2 * k1_state, jac + h / 2 * k1_jac, None, w, ucontrol2, projhyb)
-            k3_state, k3_jac = odesfun(t + h / 2, state + h / 2 * k2_state, jac + h / 2 * k2_jac, None, w, ucontrol2, projhyb)
+            k2_state, k2_jac = odesfun(ann,t + h / 2, state + h / 2 * k1_state, jac + h / 2 * k1_jac, None, w, ucontrol2, projhyb)
+            k3_state, k3_jac = odesfun(ann,t + h / 2, state + h / 2 * k2_state, jac + h / 2 * k2_jac, None, w, ucontrol2, projhyb)
         else:
-            k2_state = odesfun(t + h / 2, state + h / 2 * k1_state, None, None, w, ucontrol2, projhyb)
-            k3_state = odesfun(t + h / 2, state + h / 2 * k2_state, None, None, w, ucontrol2, projhyb)
+            k2_state = odesfun(ann,t + h / 2, state + h / 2 * k1_state, None, None, w, ucontrol2, projhyb)
+            k3_state = odesfun(ann,t + h / 2, state + h / 2 * k2_state, None, None, w, ucontrol2, projhyb)
 
         hl = h - h / 1e10
         ucontrol4 = controlfun(t + hl, batch) if controlfun is not None else []
 
         if jac is not None:
-            k4_state, k4_jac = odesfun(t + hl, state + hl * k3_state, jac + hl * k3_jac, None, w, ucontrol4, projhyb)
+            k4_state, k4_jac = odesfun(ann,t + hl, state + hl * k3_state, jac + hl * k3_jac, None, w, ucontrol4, projhyb)
         else:
-            k4_state = odesfun(t + hl, state + hl * k3_state, None, None, w, ucontrol4, projhyb)
+            k4_state = odesfun(ann,t + hl, state + hl * k3_state, None, None, w, ucontrol4, projhyb)
 
         state = state + h * (k1_state / 6 + k2_state / 3 + k3_state / 3 + k4_state / 6)
 
--- odesfun.py
+++ odesfun.py
@@ -1,29 +1,76 @@
-def odesfun(t, state, jac, hess, w, ucontrol, projhyb):
+def odesfun(ann, t, state, jac, hess, w, ucontrol, projhyb):
+
+    ann.load_state_dict({"weights": w})
 
     if jac is None and hess is None:
-        anninp = projhyb['mlm']['xfun'](t, state, ucontrol)
-        rann = projhyb['mlm']['yfun'](anninp, w, projhyb['mlm']['fundata'])
-        fstate = projhyb['userdefun_parametric_odes'](t, state, rann, ucontrol)
+        anninp = anninp_func()
+        rann = rann_func()
+        fstate = projhyb['userdefun_parametric_odes'](t, state, rann, ucontrol) # FALTA
 
         return fstate, None, None
 
     else:
 
         if projhyb['mode'] == 1:
-            anninp, DanninpDstate = projhyb['mlm']['xfun'](t, state, ucontrol)
-            rann, DrannDanninp, DrannDw = projhyb['mlm']['yfun'](anninp, w, projhyb['mlm']['fundata'])
-            fstate, DfDs, DfDrann = projhyb['userdefun_parametric_odes'](t, state, rann, ucontrol)
+            anninp = anninp_func()
+            DanninpDstate = Matrix([anninp]).jacobian(Matrix([state]))
+
+            rann = rann_func()
+            _, DrannDanninp, DrannDw = ann.backpropagate(anninp)
+
+            fstate = projhyb['userdefun_parametric_odes'](t, state, rann, ucontrol) # FALTA
+
+            DfDs = Matrix([fstate]).jacobian(Matrix([state]))
+            DfDrann = Matrix([fstate]).jacobian(Matrix([rann]))
+
             DrannDs = DrannDanninp * DanninpDstate
+
             fjac = (DfDs + DfDrann * DrannDs) * jac + DfDrann * DrannDw
 
             return fstate, fjac, None
 
         elif projhyb['mode'] == 3:
-            anninp = projhyb['mlm']['xfun'](t, state, ucontrol)
-            rann = projhyb['mlm']['yfun'](anninp, w, projhyb['mlm']['fundata'])
-            fstate, DfDs, DfDrann = projhyb['userdefun_parametric_odes'](t, state, rann, ucontrol)
+            anninp = anninp_func()
+
+            rann = rann_func()
+
+            fstate = projhyb['userdefun_parametric_odes'](t, state, rann, ucontrol)
+
+            DfDs = Matrix([fstate]).jacobian(Matrix([state]))
+            DfDrann = Matrix([fstate]).jacobian(Matrix([rann]))
+
             fjac = DfDs * jac + DfDrann
 
             return fstate, fjac, None
 
     return None, None, None
+
+     
+def anninp_func():
+    anninp = []
+
+    for i in range(1,  data["mlm"]["nx"]+1):
+        totalsyms.append(data["mlm"]["x"][str(i)]["id"])
+
+        val = sp.sympify(data["mlm"]["x"][str(i)]["val"])
+        max = sp.sympify(data["mlm"]["x"][str(i)]["max"])
+
+        anninp.append(val/max)
+
+    for i in range(1, len(anninp)+1):
+        anninp.append(anninp[i-1]/data["mlm"]["x"][str(i)]["max"])
+
+    anninp_tensor = torch.tensor(anninp, dtype=torch.float32)
+    return anninp_tensor
+
+
+def rann_func():
+    rann = []
+
+    for i in range(1, data["mlm"]["ny"]+1):
+        rann.append(sp.sympify(data["mlm"]["y"][str(i)]["id"]))
+
+    return rann
+
+
+#TODO: ASK Where fstate = projhyb['userdefun_parametric_odes'](t, state, rann, ucontrol) is defined

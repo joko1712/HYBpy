4f4ad6b1ce4712662cfd26dfe48a155a57372c3d
José Bonança Pedreira <72609286+joko1712@users.noreply.github.com>
2024-01-24 23:48:07 +0000

    Made the trainning faster

--- hybodesolver.py
+++ hybodesolver.py
@@ -5,86 +5,6 @@ import torch
 from sympy import *
 import sympy as sp
 
-
-def anninp_rann_func(projhyb):
-    species_values = extract_species_values(projhyb)
-    print("species_values:", species_values)
-
-    totalsyms = ["t", "dummyarg1", "dummyarg2", "w"]
-
-    for i in range(1, projhyb["nspecies"]+1):
-        totalsyms.append(projhyb["species"][str(i)]["id"])  # Species
-
-    for i in range(1, projhyb["ncompartments"]+1):
-        totalsyms.append(projhyb["compartment"][str(i)]["id"])  # Compartments
-
-    for i in range(1, projhyb["nparameters"]+1):
-        totalsyms.append(projhyb["parameters"][str(i)]["id"])  # Parameters
-
-    for i in range(1, projhyb["nruleAss"]+1):
-        totalsyms.append(projhyb["ruleAss"][str(i)]["id"])  # RuleAss
-
-    for i in range(1, projhyb["nreaction"]+1):
-        totalsyms.append(projhyb["reaction"][str(i)]["id"])  # Reaction
-
-    for i in range(1, projhyb["ncontrol"]+1):
-        totalsyms.append(projhyb["control"][str(i)]["id"])  # Control
-
-    anninp = []
-    anninp_mat = []    
-    rann = []
-
-    for i in range(1,  projhyb["mlm"]["nx"]+1):
-        totalsyms.append(projhyb["mlm"]["x"][str(i)]["id"])
-
-        val_expr = sp.sympify(projhyb["mlm"]["x"][str(i)]["val"])
-        max_expr = sp.sympify(projhyb["mlm"]["x"][str(i)]["max"])
-
-        anninp.append(val_expr/max_expr)
-
-
-        val_mat = val_expr.evalf(subs=species_values)
-        max_mat = max_expr.evalf(subs=species_values)
-
-
-        anninp_mat.append(val_mat/max_mat)
-
-    for i in range(1, projhyb["mlm"]["ny"]+1):
-        totalsyms.append(projhyb["mlm"]["y"][str(i)]["id"])  # Ann outputs
-        totalsyms.append(projhyb["mlm"]["y"][str(i)]["val"])
-
-        rann.append(sp.sympify(projhyb["mlm"]["y"][str(i)]["val"]))
-
-    totalsyms = symbols(totalsyms)
-
-    print("anninp:", anninp)
-
-    anninp_symbol = sp.sympify(anninp)
-
-    return anninp_symbol, rann, anninp_mat
-
-
-def extract_species_values(projhyb):
-    species_values = {}
-    for key, species in projhyb['species'].items():
-        species_id = species['id']
-        species_val = species['val']
-        species_values[species_id] = species_val
-
-    print("species_values:", species_values)
-    return species_values
-
-
-def derivativeXY(X,Y):
-    z = []
-    for i in range(0, len(Y)):
-        for j in range(0, len(X)):
-            cal = diff(X[j], Y[i])
-
-            z = z + [cal]
-    return z
-
-
 def hybodesolver(ann,odesfun, controlfun, eventfun, t0, tf, state, jac, hess, w, batch, projhyb):
     t = t0
     hopt = []
@@ -101,6 +21,7 @@ def hybodesolver(ann,odesfun, controlfun, eventfun, t0, tf, state, jac, hess, w,
     fstate = fstate_func(projhyb)
 
     anninp, rann, anninp_mat = anninp_rann_func(projhyb)
+
     anninp_tensor = torch.tensor(anninp_mat, dtype=torch.float32)
     anninp_tensor = anninp_tensor.view(-1, 1)       
 
@@ -109,13 +30,14 @@ def hybodesolver(ann,odesfun, controlfun, eventfun, t0, tf, state, jac, hess, w,
     y = activations[-1]
     
     DfDs = derivativeXY(fstate, state_symbols)
+
     DfDrann = derivativeXY(fstate, rann)
+
     values = extract_species_values(projhyb)
     values["compartment"] = int(projhyb["compartment"]["1"]["val"])
-    print("y", len(y))
     for range_y in range(0, len(y)):
         values["rann"+str(range_y+1)] = y[range_y].item()
-    print("values:", values)
+
     DfDrann =[expr.evalf(subs=values) for expr in DfDrann]
     DfDrann = np.array(DfDrann)
     DfDrann = DfDrann.reshape(len(fstate), len(rann))
@@ -125,14 +47,10 @@ def hybodesolver(ann,odesfun, controlfun, eventfun, t0, tf, state, jac, hess, w,
     DfDs = [expr.evalf(subs=values) for expr in DfDs]
     DfDs = np.array(DfDs)
     DfDs = DfDs.reshape(len(fstate), len(state_symbols))
-    #RANN = y
-    #Read y add rann(len(y)) to add numeric values to rann
     DfDs = DfDs.astype(np.float32)
     DfDs = torch.from_numpy(DfDs)
 
     fstate = [expr.evalf(subs=values) for expr in fstate]
-
-
 
     while t < tf:
         h = min(projhyb['time']['TAU'], tf - t)

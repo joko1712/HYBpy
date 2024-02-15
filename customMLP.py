Fixed state and jacobian iteration

customMLP.py
@@ -29,8 +29,8 @@ class CustomMLP(nn.Module):
         return x
 
     def get_weights(self):
-        w=[]
-        '''
+        
+        
         w = [1.2006380998802779E-5, 2.1145603844498155E-5, -2.9792708564293911E-5, 
             3.9705489251404438E-5, 1.3830384891204938E-5, -3.7745464810076942E-5, 
             1.8898710862674139E-5, 8.54514207552331E-5, -5.1638738811174825E-5, 
@@ -77,18 +77,20 @@ class CustomMLP(nn.Module):
             5.4017207288595819E-6, 0.043400002799370965]
 
         wTensor = torch.tensor(w, dtype=torch.float64)
-        '''
+        
         for layer in self.layers:
             if isinstance(layer, TanhLayer) or isinstance(layer, ReLULayer):
                 
-                layer.w.data = torch.randn_like(layer.w) * np.sqrt(2 / (layer.w.size(0) + layer.w.size(1)))
-                layer.b.data = torch.zeros_like(layer.b)
-                '''
+
+                #layer.w.data = torch.randn_like(layer.w) * np.sqrt(2 / (layer.w.size(0) + layer.w.size(1)))
+                #layer.w.data = torch.randn_like(layer.w) * 0.02 - 0.01
+                #layer.b.data = torch.zeros_like(layer.b)
+                
                 if wTensor.shape == layer.w.shape:
                     layer.w.data = wTensor
                     wTensor = wTensor[torch.numel(layer.w):]
                     layer.b.data = torch.zeros_like(layer.b)
-                '''
+                
 
             w.extend(layer.w.flatten().detach().numpy())
             w.extend(layer.b.flatten().detach().numpy())
@@ -159,8 +161,6 @@ class TanhLayer(nn.Module):
         self.w = nn.Parameter(torch.randn(output_size, input_size, dtype=torch.float64)) 
         self.b = nn.Parameter(torch.randn(output_size, 1, dtype=torch.float64))
 
-
-
     def forward(self, x):
 
         return torch.tanh(torch.mm(self.w, x) + self.b)
@@ -225,7 +225,6 @@ class Linear(nn.Module):
         super(Linear, self).__init__()
         self.w = nn.Parameter(torch.randn(output_size, input_size, dtype=torch.float64)) 
         self.b = nn.Parameter(torch.randn(output_size, 1, dtype=torch.float64))
-
 
     def forward(self, x):
         return torch.mm(self.w, x) + self.b

odesfun.py
@@ -8,7 +8,7 @@ import numpy as np
 from derivativeXY import numerical_derivativeXY
 
 
-def odesfun(ann, t, state, jac, hess, w, ucontrol, projhyb, DfDs, DfDrann, fstate, anninp, anninp_tensor, state_symbols, values):
+def odesfun(ann, t, state, jac, hess, w, ucontrol, projhyb, fstate, anninp, anninp_tensor, state_symbols, values):
 
     current_state_dict = ann.state_dict()
 
@@ -34,8 +34,33 @@ def odesfun(ann, t, state, jac, hess, w, ucontrol, projhyb, DfDs, DfDrann, fstat
 
         if projhyb['mode'] == 1:
 
+            NValues = {}
+            NValues.update(values)
+            NValues.update(state)
 
-            DanninpDstate = numerical_derivativeXY(anninp, state_symbols, values)
+            DfDs_sym = [[expr.diff(symbol) for symbol in state_symbols] for expr in fstate]
+            DfDs = [[expr.subs(NValues) for expr in row] for row in DfDs_sym]
+
+            #DfDs = numerical_derivativeXY(fstate, state_symbols, values)
+            rann_symbol = []
+            for i in range(1, projhyb["mlm"]["ny"]+1):
+                rann_symbol.append(sp.sympify(projhyb["mlm"]["y"][str(i)]["id"]))
+            
+            DfDrann_sys = [[expr.diff(symbol) for symbol in rann_symbol] for expr in fstate]
+            DfDrann = [[expr.subs(NValues) for expr in row] for row in DfDrann_sys]
+            #DfDrann = numerical_derivativeXY(fstate, rann_symbol, values)
+
+            DfDrann = np.array(DfDrann)
+            DfDrann = DfDrann.reshape(len(fstate), projhyb["mlm"]["ny"])
+            DfDrann = DfDrann.astype(np.float64)
+            DfDrann = torch.from_numpy(DfDrann)
+
+            DfDs = np.array(DfDs)
+            DfDs = DfDs.reshape(len(fstate), len(state_symbols))
+            DfDs = DfDs.astype(np.float64)
+            DfDs = torch.from_numpy(DfDs)
+
+            DanninpDstate = numerical_derivativeXY(anninp, state_symbols, NValues)
             DanninpDstate = np.array(DanninpDstate)
             DanninpDstate = DanninpDstate.reshape(len(anninp)+1, len(anninp))
             DanninpDstate = DanninpDstate.astype(np.float64)
@@ -51,8 +76,9 @@ def odesfun(ann, t, state, jac, hess, w, ucontrol, projhyb, DfDs, DfDrann, fstat
 
             fjac = torch.mm(DfDsDfDrannDrannDs,jac) + DfDrannDrannDw
 
-            fstate = [expr.evalf(subs=values) for expr in fstate]
+            fstate = [expr.evalf(subs=NValues) for expr in fstate]
 
+            print("fstate",fstate)
             return fstate, fjac
 
         elif projhyb['mode'] == 3:
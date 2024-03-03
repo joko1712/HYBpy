67e84fdee7f82de237401566c927df741474f783
José Bonança Pedreira <72609286+joko1712@users.noreply.github.com>
2024-03-03 22:48:20 +0000

    Fix weights issue

--- mlpnetsetw.py
+++ mlpnetsetw.py
@@ -4,12 +4,8 @@ import torch
 def mlpnetsetw(custom_mlp, w):
     count = 0
     for layer in custom_mlp.layers:
-        print("layer:", layer)
         input_size, output_size = layer.w.shape[1], layer.w.shape[0]
-        print("input_size:", input_size)
-        print("output_size:", output_size)
         num_weights = input_size * output_size
-        print("num_weights:", num_weights)
         num_biases = output_size
 
         layer_weights = w[count:count +

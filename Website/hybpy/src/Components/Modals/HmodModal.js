b7113d7a31dd49d40e99d1925dfd9618b1e7efa6
José Bonança Pedreira <72609286+joko1712@users.noreply.github.com>
2024-07-17 17:39:01 +0100

    Small fixes for presentation

--- Website/hybpy/src/Components/Modals/HmodModal.js
+++ Website/hybpy/src/Components/Modals/HmodModal.js
@@ -8,8 +8,10 @@ import {
     Grid,
     MenuItem,
     TextField,
+    Tooltip,
 } from "@mui/material";
-
+import InfoIcon from "@mui/icons-material/Info";
+import IconButton from "@mui/material/IconButton";
 const HmodModal = ({ open, handleClose, handleSave, initialValues, setHmodOptions }) => {
     const [hiddenNodes, setHiddenNodes] = useState(initialValues ? initialValues.hiddenNodes : "");
     const [layer, setLayer] = useState(initialValues ? initialValues.layer : 1);
@@ -66,57 +68,98 @@ const HmodModal = ({ open, handleClose, handleSave, initialValues, setHmodOption
                 <Grid container spacing={2} marginTop={2}>
                     <Grid item xs={12}>
                         <TextField
-                            fullWidth
+                            style={{ width: "90%" }}
                             label='Hidden Nodes'
                             value={hiddenNodes}
                             onChange={(e) => setHiddenNodes(e.target.value)}
                             helperText="Enter hidden nodes as space-separated integers (e.g., '5 5' for 2 layers with 5 nodes each)"
                         />
+                        <Tooltip
+                            title='This specifies the number of neurons in each hidden layer of the neural network.'
+                            arrow>
+                            <IconButton size='small'>
+                                <InfoIcon />
+                            </IconButton>
+                        </Tooltip>
                     </Grid>
+
                     <Grid item xs={12}>
                         <TextField
-                            fullWidth
                             label='Layer'
                             select
+                            style={{ width: "90%" }}
                             value={layer}
                             onChange={(e) => setLayer(Number(e.target.value))}
                             helperText='Activation function'>
                             <MenuItem value={1}>Tanh</MenuItem>
                             <MenuItem value={2}>ReLu</MenuItem>
+                            <MenuItem value={3}>LSTMLayer</MenuItem>
                         </TextField>
+                        <Tooltip
+                            title='Tanh: Effective in neural networks for its ability to center data around zero. ReLU: Effective in avoiding the vanishing gradient problem make it ideal for deep networks. LSTMLayer: Powerful for tasks involving sequences, such as time-series prediction by introducing a memory cell that can maintain its state over long periods.'
+                            arrow>
+                            <IconButton size='small'>
+                                <InfoIcon />
+                            </IconButton>
+                        </Tooltip>
                     </Grid>
                     <Grid item xs={12}>
                         <TextField
-                            fullWidth
+                            style={{ width: "90%" }}
                             label='Method'
                             select
                             value={method}
                             onChange={(e) => setMethod(Number(e.target.value))}
-                            helperText='Trainning Method: Trust Region Reflective(trf), trust-constr, ADAM'>
-                            <MenuItem value={1}>trf</MenuItem>
-                            <MenuItem value={2}>trust-constr</MenuItem>
-                            <MenuItem value={3}>ADAM</MenuItem>
+                            helperText='Training Method: Trust Region Reflective(trf), trust-constr, Simulated Annealing, ADAM'>
+                            <MenuItem value={1}>TRF</MenuItem>
+
+                            <MenuItem value={2}>Trust-Constr</MenuItem>
+
+                            <MenuItem value={3}>Simulated Annealing</MenuItem>
+
+                            <MenuItem value={4}>ADAM</MenuItem>
                         </TextField>
+                        <Tooltip
+                            title='Trust Region Reflective: Suitable for large-scale nonlinear least-squares problems. Trust-Region Constrained or L-BFGS-B: Suitable for constrained optimization problems. Simulated Annealing: Probabilistic technique for approximating the global optimum. ADAM: Optimization algorithm that computes adaptive learning rates for each parameter.'
+                            arrow>
+                            <IconButton size='small'>
+                                <InfoIcon />
+                            </IconButton>
+                        </Tooltip>
                     </Grid>
                     <Grid item xs={12}>
                         <TextField
-                            fullWidth
+                            style={{ width: "90%" }}
                             label='Niter'
                             type='number'
                             value={niter}
                             onChange={(e) => setNiter(Number(e.target.value))}
                             helperText='Number of integrations'
                         />
+                        <Tooltip
+                            title='Number of iterations: Defines how many times the learning algorithm will work through the entire training dataset.'
+                            arrow>
+                            <IconButton size='small'>
+                                <InfoIcon />
+                            </IconButton>
+                        </Tooltip>
                     </Grid>
                     <Grid item xs={12}>
                         <TextField
-                            fullWidth
+                            style={{ width: "90%" }}
                             label='Nstep'
                             type='number'
                             value={nstep}
                             onChange={(e) => setNstep(Number(e.target.value))}
                             helperText='Number of re-initializations'
                         />
+                        <Tooltip
+                            title='Number of re-initializations: Specifies how many times the model will re-initialize the weights during training to find the optimal values.'
+                            arrow>
+                            <IconButton size='small'>
+                                <InfoIcon />
+                            </IconButton>
+                        </Tooltip>
                     </Grid>
                     <Grid item xs={12}>
                         <Button
@@ -134,7 +177,7 @@ const HmodModal = ({ open, handleClose, handleSave, initialValues, setHmodOption
                         <>
                             <Grid item xs={12}>
                                 <TextField
-                                    fullWidth
+                                    style={{ width: "90%" }}
                                     label='Jacobian'
                                     select
                                     value={jacobian}
@@ -143,10 +186,18 @@ const HmodModal = ({ open, handleClose, handleSave, initialValues, setHmodOption
                                     <MenuItem value={0}>Off</MenuItem>
                                     <MenuItem value={1}>On</MenuItem>
                                 </TextField>
+                                <Tooltip
+                                    title='Direct calculation of Jacobian: Enables or disables the direct calculation of the Jacobian matrix.'
+                                    arrow>
+                                    <IconButton size='small'>
+                                        <InfoIcon />
+                                    </IconButton>
+                                </Tooltip>
                             </Grid>
+
                             <Grid item xs={12}>
                                 <TextField
-                                    fullWidth
+                                    style={{ width: "90%" }}
                                     label='Hessian'
                                     select
                                     value={hessian}
@@ -155,33 +206,59 @@ const HmodModal = ({ open, handleClose, handleSave, initialValues, setHmodOption
                                     <MenuItem value={0}>Off</MenuItem>
                                     <MenuItem value={1}>On</MenuItem>
                                 </TextField>
+                                <Tooltip
+                                    title='Direct calculation of Hessian: Enables or disables the direct calculation of the Hessian matrix.'
+                                    arrow>
+                                    <IconButton size='small'>
+                                        <InfoIcon />
+                                    </IconButton>
+                                </Tooltip>
                             </Grid>
+
                             <Grid item xs={12}>
                                 <TextField
-                                    fullWidth
+                                    style={{ width: "90%" }}
                                     label='TAU'
                                     type='number'
                                     value={tau}
                                     onChange={(e) => setTau(Number(e.target.value))}
                                     helperText='Enter a floating-point value'
                                 />
+                                <Tooltip
+                                    title='A parameter that influences the convergence speed of the optimization process.'
+                                    arrow>
+                                    <IconButton size='small'>
+                                        <InfoIcon />
+                                    </IconButton>
+                                </Tooltip>
                             </Grid>
+
                             <Grid item xs={12}>
                                 <TextField
-                                    fullWidth
+                                    style={{ width: "90%" }}
                                     label='Mode'
                                     select
                                     value={mode}
                                     onChange={(e) => setMode(Number(e.target.value))}
-                                    helperText='Trainning Mode: Indirect, Direct, Semidirect'>
+                                    helperText='Trainning Mode: Indirect, Direct, Semidirect. Direct mode not yet implemented.'>
                                     <MenuItem value={1}>Indirect</MenuItem>
-                                    <MenuItem value={2}>Direct</MenuItem>
+                                    <MenuItem value={2} disabled>
+                                        Direct
+                                    </MenuItem>
                                     <MenuItem value={3}>Semidirect</MenuItem>
                                 </TextField>
+                                <Tooltip
+                                    title='Indirect: Separates the calculation of residuals and Jacobians from forward simulation. Direct: Fully integrates the calculation using automatic differentiation (not yet implemented). Semidirect: Combines aspects of both indirect and direct modes for efficient computation.'
+                                    arrow>
+                                    <IconButton size='small'>
+                                        <InfoIcon />
+                                    </IconButton>
+                                </Tooltip>
                             </Grid>
+
                             <Grid item xs={12}>
                                 <TextField
-                                    fullWidth
+                                    style={{ width: "90%" }}
                                     label='Bootstrap'
                                     select
                                     value={bootstrap}
@@ -190,6 +267,13 @@ const HmodModal = ({ open, handleClose, handleSave, initialValues, setHmodOption
                                     <MenuItem value={0}>On</MenuItem>
                                     <MenuItem value={1}>Off</MenuItem>
                                 </TextField>
+                                <Tooltip
+                                    title='Enables or disables the use of bootstrap sampling, which is a method for estimating the distribution of a statistic by resampling with replacement.'
+                                    arrow>
+                                    <IconButton size='small'>
+                                        <InfoIcon />
+                                    </IconButton>
+                                </Tooltip>
                             </Grid>
                         </>
                     )}
--- hybtrain.py
+++ hybtrain.py
@@ -88,7 +88,6 @@ def hybtrain(projhyb, file, user_id):
     print("\nTraining method:")
 
     mode = projhyb['mode']
-    # INDIRECT IS THE ONLY THAT IS IMPLEMENTED
     if mode == 1:
         print("   Mode:                   Indirect")
 
@@ -139,6 +138,11 @@ def hybtrain(projhyb, file, user_id):
         print("   Control function:       OFF")
         print("   ASK USER TO DEFINE CONTROL FUNCTION")
 
+    if projhyb["crossval"] == 1:
+        print("   Cross-validation:       ON")
+    else:
+        print("   Cross-validation:       OFF")
+
 #######################################################################################################################
     options = {}
     species_bounds = []
@@ -156,9 +160,9 @@ def hybtrain(projhyb, file, user_id):
     projhyb["mlm"]['nw'] = (projhyb["mlm"]['nx'] + 1) * projhyb["mlm"]['nh'][0]
 
     if projhyb['method'] == 1:
-        print("   Optimiser:              Levenberg-Marquardt")
+        print("   Optimiser:              Trust Region Reflective")
         options = {
-            'xtol': 1e-8, #1e-10
+            'xtol': 1e-15, #1e-10
             'verbose': projhyb['display'],
             'max_nfev': 100 * projhyb['niter'] * projhyb['niteroptim'],
             'method': 'trf',
@@ -174,7 +178,7 @@ def hybtrain(projhyb, file, user_id):
                 'maxiter':100 * projhyb['niter'] * projhyb['niteroptim']
             } 
         }
-    elif projhyb['method'] == 4:
+    elif projhyb['method'] == 3:
         print("   Optimiser:              Simulated Annealing")
         bounds = species_bounds * projhyb['mlm']['nw']
         options = {
@@ -182,7 +186,7 @@ def hybtrain(projhyb, file, user_id):
             'verbose': projhyb['display']
         }
 
-    elif projhyb['method'] == 3:
+    elif projhyb['method'] == 4:
         print("   Optimiser:              Adam")
         num_epochs = 100  
         lr = 0.001  
@@ -236,6 +240,14 @@ def hybtrain(projhyb, file, user_id):
 
         evaluator = IndirectFunctionEvaluator(ann, projhyb, file, resfun_indirect_jac)
 
+    elif projhyb['mode'] == 2:
+            
+        evaluator = IndirectFunctionEvaluator(ann, projhyb, file, resfun_direct_jac)
+    
+    elif projhyb['mode'] == 3:
+            
+        evaluator = IndirectFunctionEvaluator(ann, projhyb, file, resfun_semidirect_jac)
+
 #######################################################################################################################
     
     for istep in range(1, projhyb['nstep']):
@@ -265,7 +277,7 @@ def hybtrain(projhyb, file, user_id):
 
         if projhyb['hessian'] == 1:
             options['hess'] = evaluator.hess_func
-        
+    '''
         if projhyb["method"] == 1:  # LEVENBERG-MARQUARDT
             print("optios", options)
             print("weights", weights)
@@ -295,6 +307,7 @@ def hybtrain(projhyb, file, user_id):
             optimized_weights = adam_optimizer_train(ann, projhyb, evaluator, num_epochs, lr)
     
     '''
+    '''
     w = [-1.30284588e-03, -1.14245236e-02, -7.91831059e-04,  4.40199659e-03,
             1.72756969e-03, -5.00282668e-03, -2.01651446e-03, -3.93168300e-03,
             9.56139266e-04, -1.25006042e-03, -1.57937766e-03, -2.50723996e-03,
@@ -304,7 +317,48 @@ def hybtrain(projhyb, file, user_id):
             6.72249841e-06,  4.22301199e-02]
     '''
     
+    w = [ 1.51872094e-03, -1.59585915e-03, -7.12253433e-04,  1.24342755e-04,
+            7.74540120e-04,  1.99441788e-02, -1.26969915e-02, -3.67767926e-03,
+            -1.40537984e-03, -5.17297002e-03, -1.85277002e-03,  1.74874954e-03,
+            6.56016046e-04, -1.54538042e-04, -5.62466811e-04,  2.64828828e-02,
+            -1.29078782e-02, -2.86998296e-03, -3.28937350e-03, -1.46798282e-02,
+            1.08838826e-02,  8.35603951e-03,  4.62468320e-03, -4.93299581e-03,
+            -3.38708910e-02,  9.56367378e-03, -8.93223669e-03, -3.34692422e-03,
+            5.63855163e-04,  2.80681532e-03,  2.34064976e-03, -4.90464784e-03,
+            -2.41182354e-03,  1.33099790e-03,  5.88513180e-03,  4.52984615e-03,
+            -4.60275981e-03, -1.85567481e-03, 4.55726909e-04,  2.12187582e-03,
+            -3.07892454e-03,  3.05248249e-03,  1.20104149e-03, -3.40335053e-04,
+            -1.44833498e-03,  1.40478742e-03, -1.47642337e-03, -6.05061645e-04,
+            1.23577693e-04,  6.19980360e-04,  9.93972677e-06, -1.17452681e-04,
+            -3.83150019e-06,  9.81991234e-05,  1.88650875e-04, -2.61882162e-08,
+            -5.74176106e-07,  4.48708336e-07,-1.96318907e-07,  5.34379366e-07,
+            -9.05059523e-04, -3.78891547e-03,  9.28477323e-04, -1.22795051e-03,
+            5.03147974e-03, -1.02264114e-02, -1.03661777e-02, -3.96977224e-03,
+            4.06746594e-03,  1.40531047e-02, -1.66942884e-02,  8.71160820e-03,
+            -1.79788117e-02,  1.99990204e-02, -9.71130284e-03, -1.26890938e-03,
+            8.88607362e-03, -3.87793796e-03,  5.82036485e-03, -1.08326037e-02,
+            6.17182015e-03, -1.52597979e-02,  1.01063687e-02, -1.36405502e-02,
+            1.84950105e-02,  8.53358615e-05,  3.37067528e-05,  3.55832699e-05,
+            -8.44830702e-05,  4.73012733e-05, -2.19510522e-04,  1.01640910e-03,
+            4.29801868e-02,  4.68624654e-05,  2.67207005e-05, -2.18452390e-02,
+            8.78507425e-06, -1.62294776e-04,  5.69775024e-04, -1.32757487e-02,
+            6.36695478e-05, -1.50478056e-05,  1.04180612e-02, -4.69567708e-05,
+            -1.05798717e-05, -4.99704584e-05,  2.04628426e-03, -1.88611539e-05,
+            5.00014177e-05, -2.44433960e-02, -2.20355572e-05,  9.94252867e-05,
+            -2.14544245e-04, -2.20562387e-03, -2.46881624e-05,  3.35512951e-05,
+            -2.15364604e-03,  5.72864861e-06,  1.70329603e-06, -1.20269089e-04,
+            -2.59561229e-02,  5.50277599e-05,  6.37817347e-05,  4.35776302e-02,
+            4.96333638e-05,  1.54874169e-01,  1.70770553e-01,  3.46536551e-05,
+            1.00330855e+00,  9.96498529e-02,  6.14672585e-06,  4.25715988e-02]
+    '''
+    w =  [9.84452500e-04, 3.50165149e-05, 1.15788796e-02, 2.94717984e-04,
+            2.61447847e-04, 3.10083638e-01, 9.53791945e-02, 3.72253961e-01]
+
+    '''
+
     optimized_weights = w   
+
+    
     
     testing = teststate(ann, user_id, projhyb, file, optimized_weights, projhyb['method'])
 
@@ -474,6 +528,182 @@ def resfun_indirect_jac(ann, w, istrain, projhyb, file, method=1):
                      
     return fobj, jac
 
+####
+#   SEMIDIRECT
+####
+
+def resfun_semidirect_jac(ann, w, istrain, projhyb, file, method=1):
+    print("weights", w)
+
+    ann.set_weights(w)
+    ann.print_weights_and_biases()
+    if not istrain:
+        istrain = projhyb["istrain"]
+
+    ns = projhyb["nspecies"]
+    nt = ns + projhyb["ncompartment"]
+    nw = projhyb["mlm"]["nw"]
+    isres = []
+    isresY = []
+    for i in range(1, ns + 1):
+        if projhyb["species"][str(i)]["isres"] == 1:
+            isres = isres + [i]
+            isresY = isresY + [i - 1]
+
+    isres = isres
+
+    nres = len(isres)
+
+    npall = sum(file[i+1]["np"] for i in range(file["nbatch"]) if file[i+1]["istrain"] == 1)
+
+    sresall = np.zeros(npall * nres)
+
+    sjacall = np.zeros((npall * nres, nw))
+
+    CONT = 0
+
+    for l in range(file["nbatch"]):
+        l = l + 1
+        if file[l]["istrain"] == 1:
+            tb = file[l]["time"]
+            Y = file[l]["y"]
+            Y = np.array(Y)
+            Y = Y.astype(np.float64)
+            Y = torch.from_numpy(Y)
+
+            batch = str(l)
+
+            sY = file[l]["sy"]
+            sY = np.array(sY)
+            sY = sY.astype(np.float64)
+            sY = torch.from_numpy(sY)
+
+            state = np.array(file[l]["y"][0])
+            Sw = np.zeros((nt, nw))
+            jac = np.zeros((nt, projhyb["mlm"]["ny"]))
+
+            for i in range(1, file[l]["np"]):
+                statedict = np.array(file[l]["key_value_array"][i-1])
+                print("statedict", statedict)
+
+                batch_data = file[l]
+                _, state, Sw, hess = hybodesolver(ann, odesfun,
+                                            control_function, projhyb["fun_event"], tb[i-1], tb[i],
+                                            state, statedict, Sw, 0, w, batch_data, projhyb)
+
+                ucontrol = control_function(tb[i], batch_data)
+                inp = projhyb["mlm"]["xfun"](tb[i], state, ucontrol)
+                _, _, DrannDw = projhyb["mlm"]["yfun"](inp, w, projhyb["mlm"]["fundata"])
+
+                Sw = np.dot(jac, DrannDw)
+                resall[COUNT:COUNT + nres] = (Y[i, isres] - state[isres]) / sY[i, isres]
+                jacall[COUNT:COUNT+nres, :nw] = -Sw[isres, :] / np.tile(sY[i, isres], (nw, 1)).T
+                COUNT += nres
+    
+    valid_idx = ~np.isnan(sresall) & ~np.isinf(sresall)
+    sresall = sresall[valid_idx]
+    sjacall = sjacall[valid_idx, :]
+
+    if method == 1 or method == 4:
+        fobj = sresall
+        jac = sjacall
+    else:
+        fobj = np.dot(sresall, sresall) / len(sresall)
+        jac = np.sum(2 * np.repeat(sresall.reshape(-1, 1), nw,
+                     axis=1) * sjacall, axis=0) / len(sresall)
+
+
+####
+#   DIRECT
+####
+
+def resfun_direct_jac(ann, w, istrain, projhyb, file, method=1):
+    print("weights", w)
+
+    # LOAD THE WEIGHTS into the ANN
+    ann.set_weights(w)
+    ann.print_weights_and_biases()
+    if not istrain:
+        istrain = projhyb["istrain"]
+
+    ns = projhyb["nspecies"]
+    nt = ns + projhyb["ncompartment"]
+    nw = projhyb["mlm"]["nw"]
+    isres = []
+    isresY = []
+    for i in range(1, ns + 1):
+        if projhyb["species"][str(i)]["isres"] == 1:
+            isres = isres + [i]
+            isresY = isresY + [i - 1]
+
+    isres = isres
+
+    nres = len(isres)
+
+    npall = sum(file[i+1]["np"] for i in range(file["nbatch"]) if file[i+1]["istrain"] == 1)
+
+    sresall = np.zeros(npall * nres)
+    sjacall = np.zeros((npall * nres, nw))
+
+    COUNT = 0
+    for l in range(file["nbatch"]):
+        l = l + 1
+        if file[l]["istrain"] == 1:
+            tb = file[l]["time"]
+            Y = file[l]["y"]
+            Y = np.array(Y)
+            Y = Y.astype(np.float64)
+            Y = torch.from_numpy(Y)
+
+            batch = str(l)
+
+            sY = file[l]["sy"]
+            sY = np.array(sY)
+            sY = sY.astype(np.float64)
+            sY = torch.from_numpy(sY)
+
+            state = np.array(file[l]["y"][0])
+            state = torch.tensor(state, requires_grad=True, dtype=torch.float64)
+
+            for i in range(1, file[l]["np"]):
+                statedict = np.array(file[l]["key_value_array"][i-1])
+                print("statedict", statedict)
+
+                batch_data = file[l]
+                _, state, Sw, hess = hybodesolver(ann, odesfun,
+                                            control_function , projhyb["fun_event"], tb[i-1], tb[i],
+                                            state, statedict, Sw, 0, w, batch_data, projhyb)
+
+                Y_select = Y[i, isresY]
+                state_tensor = torch.tensor(state, dtype=torch.float64)
+                state_adjusted = state_tensor[0:nres]
+                Ystate = Y_select - state_adjusted
+
+                sresall[COUNT:COUNT + nres] = Ystate.detach().numpy() / sY[i, isresY].numpy()
+
+                Ystate.sum().backward()
+                sjacall[COUNT:COUNT + nres, 0:nw] = state.grad[isresY, :].detach().numpy() / sY[i, isresY].numpy()
+                state.grad.zero_()
+
+                COUNT += nres
+                print("#################################################")
+                print("------------------LOOP", i, "------------------")
+                print("#################################################")
+                print("state", state)
+
+    valid_idx = ~np.isnan(sresall) & ~np.isinf(sresall)
+    sresall = sresall[valid_idx]
+    sjacall = sjacall[valid_idx, :]
+
+    if method == 1 or method == 4:
+        fobj = sresall
+        jac = sjacall
+    else:
+        fobj = np.dot(sresall.T, sresall) / len(sresall)
+        jac = np.sum(2 * np.repeat(sresall.reshape(-1, 1), nw,
+                     axis=1) * sjacall, axis=0) / len(sresall)
+
+    return fobj, jac
 
 def adam_optimizer_train(ann, projhyb, evaluator, num_epochs, lr):
     weights, _ = ann.get_weights()
@@ -631,7 +861,7 @@ def teststate(ann, user_id, projhyb, file, w, method=1):
         'r2_train': [],
         'r2_test': []
     }
-
+    print("ranfe", projhyb['mlm']['nx'])
     for i in range(projhyb['mlm']['nx']):
         actual_train = []
         actual_test = []
